a1ebfe9b89e65fbb4029ad7238d7a917409ad3c9
안호진 <asdv94@naver.com>
2022-06-20 13:55:45 +0900

    BOJ용 input 모듈 추가

---
+++ boj/BOJ입출력.js
@@ -0,0 +1,19 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = []; // input 배열 선언
+
+//Ctrl+c 또는 Ctrl+d로 입력 종료
+rl.on("line", function (line) {
+  input.push(line); // 입력받는 각 줄의 값을 input 배열에 저장
+}).on("close", function () {
+  solution(input);
+  process.exit();
+});
+/////////////////////////////////////////////////////////////////////////////////////
+// 이하 fs 모듈 이용
+const fs = require("fs");
+const inputList = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
