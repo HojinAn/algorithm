657215b0f4849ec579ef3c9f9d4f5011cf86c137
안호진 <asdv94@naver.com>
2022-06-21 11:20:10 +0900

    백준 입출력 주석 추가

--- boj/BOJ입출력.js
+++ boj/BOJ입출력.js
@@ -1,19 +1,21 @@
+// Ctrl+c 또는 Ctrl+d로 입력 종료
+// readline 모듈 이용
 const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
-let input = []; // input 배열 선언
-
-//Ctrl+c 또는 Ctrl+d로 입력 종료
+const input = []; // input 배열 선언
 rl.on("line", function (line) {
   input.push(line); // 입력받는 각 줄의 값을 input 배열에 저장
 }).on("close", function () {
   solution(input);
   process.exit();
 });
-/////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////
+// 다른 방법
+///////////////////////////////////////////////////
 // 이하 fs 모듈 이용
 const fs = require("fs");
 const inputList = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+// 입력받는 문자열 자체를 개행 기준으로 split해서 input에 저장
