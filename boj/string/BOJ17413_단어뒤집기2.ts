70760211af1c22587ee8fd518abd8ffa6358c449
HojinAn <asdv94@naver.com>
2022-08-06 19:16:31 +0900

    [BOJ] 단어 뒤집기 2 / S3 / https://www.acmicpc.net/problem/17413

--- boj/string/BOJ17413_단어뒤집기2.ts
+++ boj/string/BOJ17413_단어뒤집기2.ts
@@ -1,23 +1,31 @@
+// import * as fs from "fs";
+// const str = fs.readFileSync("/dev/stdin").toString().trim(),
+// { length } = str,
+// ans: string[] = [];
+// for (let i = 0; i < length; i++) {
+//   const el: string[] = [];
+//   switch (str[i]) {
+//     case "<":
+//       while (str[i] !== ">") ans.push(str[i++]);
+//       ans.push(str[i++]);
+//       break;
+//       case " ":
+//         ans.push(str[i++]);
+//         break;
+//         default:
+//           while (str[i] !== " " && str[i] !== "<" && i < length) el.push(str[i++]);
+//           break;
+//         }
+//         i--;
+//         el.reverse();
+//         el.length && ans.push(el.join(""));
+//       }
+//       console.log(ans.join(""));
+
 import * as fs from "fs";
-const str = fs.readFileSync("/dev/stdin").toString().trim(),
-  { length } = str,
-  ans: string[] = [];
-for (let i = 0; i < length; i++) {
-  const el: string[] = [];
-  switch (str[i]) {
-    case "<":
-      while (str[i] !== ">") ans.push(str[i++]);
-      ans.push(str[i++]);
-      break;
-    case " ":
-      ans.push(str[i++]);
-      break;
-    default:
-      while (str[i] !== " " && str[i] !== "<" && i < length) el.push(str[i++]);
-      break;
-  }
-  i--;
-  el.reverse();
-  el.length && ans.push(el.join(""));
-}
-console.log(ans.join(""));
+const str = fs.readFileSync("/dev/stdin").toString().trim();
+console.log(
+  str.replace(/<[a-z0-9 ]+>|[a-z0-9]+/g, (match) =>
+    match.startsWith("<") ? match : match.split("").reverse().join("")
+  )
+);
