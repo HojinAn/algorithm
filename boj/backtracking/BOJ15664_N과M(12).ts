bb90f7a8d1620af10c435b6850d17e4013a644ff
HojinAn <asdv94@naver.com>
2022-09-04 13:02:11 +0900

    [BOJ] N과 M (12) / S2 / https://www.acmicpc.net/problem/15666

---
+++ boj/backtracking/BOJ15664_N과M(12).ts
@@ -0,0 +1,18 @@
+import * as fs from "fs";
+const [[n, m], nums] = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((el) => el.trim().split(" ").map(Number));
+const ans = new Set<string>();
+nums.sort((a, b) => a - b);
+const repetCombi = (cnt: number, start: number, arr: number[]) => {
+  if (cnt === m) {
+    ans.add(arr.join(" "));
+    return;
+  }
+  for (let i = start; i < n; i++) repetCombi(cnt + 1, i, [...arr, nums[i]]);
+};
+repetCombi(0, 0, []);
+console.log(Array.from(ans).join("\n"));
