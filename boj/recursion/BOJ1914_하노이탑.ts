c1a39cbc899529af743579489ab60ffc364f44d0
안호진 <asdv94@naver.com>
2023-01-28 23:51:54 +0900

    [BOJ] 하노이 탑 / S1 / https://www.acmicpc.net/problem/1914

--- boj/recursion/BOJ1914_하노이탑.ts
+++ boj/recursion/BOJ1914_하노이탑.ts
@@ -1,2 +1,21 @@
 import * as fs from 'fs';
-const n = Number(fs.readFileSync('/dev/stdin').toString());
+const n = +fs.readFileSync('/dev/stdin').toString();
+
+if (n <= 20) {
+  const ans = <string[]>[];
+  const hanoi = (size: number, [from, via, to]: number[]) =>
+    size === 1
+      ? ans.push(`${from} ${to}`)
+      : (hanoi(size - 1, [from, to, via]),
+        hanoi(1, [from, via, to]),
+        hanoi(size - 1, [via, from, to]));
+  hanoi(n, [1, 2, 3]);
+  console.log(ans.length);
+  console.log(ans.join('\n'));
+} else {
+  const ONE = BigInt(1);
+  const TWO = BigInt(2);
+  const hanoi = (n: number): bigint =>
+    n === 1 ? ONE : TWO * hanoi(n - 1) + ONE;
+  console.log(`${hanoi(n)}`);
+}
