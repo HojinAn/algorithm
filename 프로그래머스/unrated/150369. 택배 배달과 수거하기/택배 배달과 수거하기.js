8aca45462983ad8bae54aedd4451901bffa2a046
안호진 <47904304+HojinAn@users.noreply.github.com>
2023-02-23 09:50:48 +0900

    [unrated] Title: 택배 배달과 수거하기, Time: 68.06 ms, Memory: 64.7 MB -BaekjoonHub

--- 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
+++ 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
@@ -1,33 +1,40 @@
 function solution(cap, n, deliveries, pickups) {
-    const deliStack = deliveries.reduce((st, cnt, i) => {
-        cnt && st.push([cnt, i + 1]);
-        return st;
-    }, [])
-    const pickStack = pickups.reduce((st, cnt, i) => {
-        cnt && st.push([cnt, i + 1]);
-        return st;
-    }, [])
-    let ans = 0 ;
+    const stackReducer = (stack, no, i) => {
+        no && stack.push([no, i + 1]);
+        return stack;
+    }
+    const deliStack = deliveries.reduce(stackReducer, [])
+    const pickStack = pickups.reduce(stackReducer, []);
+    let movingDist = 0;
+    
     while (1) {
-        const deliLen = deliStack.length;
-        const pickLen = pickStack.length;
-        if (!deliLen && !pickLen) break;
-        ans += 2 * Math.max(
-            deliLen ? deliStack[deliLen - 1][1] : 0,
-            pickLen ? pickStack[pickLen - 1][1] : 0
-        );
-        let deliCnt = 0;
-        while (deliCnt <= cap && deliStack.length) {
-            const [cnt, idx] = deliStack.pop();
-            if (cnt + deliCnt > cap) deliStack.push([deliCnt + cnt - cap, idx]);
-            deliCnt += cnt;
+        let deliCnt = cap;
+        let pickCnt = cap;
+        const deliSize = deliStack.length;
+        const pickSize = pickStack.length;
+        
+        if (deliSize === 0 && pickSize === 0) break;
+        movingDist += 2 * Math.max(deliSize ? deliStack[deliSize - 1][1] : 0, pickSize ? pickStack[pickSize - 1][1] : 0);
+        
+        while (deliStack.length) {
+        	const [cnt, idx] = deliStack.pop();
+            const diff = cnt - deliCnt;
+            if (diff > 0) {
+                deliStack.push([diff, idx])
+                break;
+            }
+            deliCnt -= cnt;
         }
-        let pickCnt = 0;
-        while (pickCnt <= cap && pickStack.length) {
-            const [cnt, idx] = pickStack.pop();
-            if (cnt + pickCnt > cap) pickStack.push([pickCnt + cnt - cap, idx]);
-            pickCnt += cnt;
+        while (pickStack.length) {
+        	const [cnt, idx] = pickStack.pop();
+            const diff = cnt - pickCnt;
+            if (diff > 0) {
+                pickStack.push([diff, idx])
+                break;
+            }
+            pickCnt -= cnt;
         }
     }
-    return ans;
+    
+    return movingDist;
 }
