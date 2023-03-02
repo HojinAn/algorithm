304dd0b1df7a9b49317a43759c03826109eaebc6
안호진 <47904304+HojinAn@users.noreply.github.com>
2023-03-02 11:25:34 +0900

    [Gold I] Title: 할 일 정하기 1, Time: 804 ms, Memory: 175508 KB -BaekjoonHub

--- 백준/Gold/1311. 할 일 정하기 1/할 일 정하기 1.ts
+++ 백준/Gold/1311. 할 일 정하기 1/할 일 정하기 1.ts
@@ -9,21 +9,18 @@ const costs = inp.map((li) => li.trim().split(' ').map(Number));
 
 const cache = [...Array(n)].map(() => Array(1 << n).fill(INF));
 
-for (let j = 0; j < n; j++) cache[0][1 << j] = costs[0][j];
-
-for (let i = 1; i < n; i++) {
-  for (let j = 0; j < 1 << n; j++) {
-    for (let k = 0; k < n; k++) {
-      if (!(j & (1 << k))) {
-        cache[i][j | (1 << k)] = Math.min(
-          costs[i][k] + cache[i - 1][j],
-          cache[i][j | (1 << k)]
-        );
-      }
+const dp = (cur: number, bit: number) => {
+  if (cur >= n) return 0;
+  if (cache[cur][bit] !== INF) return cache[cur][bit];
+  for (let i = 0; i < n; i++) {
+    if (!(bit & (1 << i))) {
+      cache[cur][bit] = Math.min(
+        cache[cur][bit],
+        costs[cur][i] + dp(cur + 1, bit | (1 << i))
+      );
     }
   }
-}
+  return cache[cur][bit];
+};
 
-console.log(
-  cache[n - 1].reduce((max, no) => (no === INF ? max : Math.max(max, no)), 0)
-);
+console.log(dp(0, 0));
