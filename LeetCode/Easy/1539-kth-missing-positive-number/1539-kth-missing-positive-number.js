c1998554b43f92df3cad03c8d8034723228b05c9
안호진 <47904304+HojinAn@users.noreply.github.com>
2023-03-06 09:09:00 +0900

    Time: 62 ms (78.09%), Space: 44.4 MB (11.60%) - LeetHub

---
+++ LeetCode/Easy/1539-kth-missing-positive-number/1539-kth-missing-positive-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+function findKthPositive (arr, k) {
+    const n = arr.length;
+    const existSet = new Set(arr);
+    const ans = [];
+    let no = 1;
+    while(ans.length < k) {
+        if (!existSet.has(no)) ans.push(no)
+        no++;
+    }
+    return ans[k - 1];
+};
