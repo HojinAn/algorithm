ffc9cdacaf67083ba755af67a24ed66abdf39a32
안호진 <47904304+HojinAn@users.noreply.github.com>
2023-03-09 17:37:25 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 5.39 ms, Memory: 40.8 MB -BaekjoonHub

--- 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
+++ 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
@@ -1,23 +1,17 @@
 function solution(arr) {
     const n = arr.length;
-    
     const ans = [0, 0];
     
     const checkAllSame = (r, c, len, el = arr[r][c]) => {
         for (let i = r; i < r + len; i++)
             for (let j = c; j < c + len; j++)
                 if (el !== arr[i][j]) return false;
-
         return true;
     }
     
-    const divide = (r, c, len, el = arr[r][c]) => {
-        if (len === 1) {
-            ans[el]++;
-            return;
-        }
-    	if (checkAllSame(r, c, len)) {
-			ans[el]++;
+    const divide = (r, c, len) => {
+        if (len === 1 || checkAllSame(r, c, len)) {
+			ans[arr[r][c]]++;
             return;
         }
         const half = len >> 1;
@@ -28,5 +22,6 @@ function solution(arr) {
     }
     
 	divide(0, 0, n);
+    
     return ans;
 }
