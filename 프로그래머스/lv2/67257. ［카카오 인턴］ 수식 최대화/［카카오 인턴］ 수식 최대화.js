83f86c0a6697a35634e58fe0bf1c2d53124fdbb2
안호진 <47904304+HojinAn@users.noreply.github.com>
2023-03-28 23:49:52 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.75 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.js
+++ 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.js
@@ -9,8 +9,6 @@ function solution(expression) {
     const MINUS = '-';
     const OP = [PLUS, MULTI, MINUS];
     
-    let max = 0;
-    
     const calc = (a, b, op) => {
         switch(op) {
             case PLUS:
@@ -23,30 +21,27 @@ function solution(expression) {
     }
     
     const doCalc = (exp, op) => {
-        const expCopy = [exp[0]];
+        const expCopy = [];
         let n = exp.length;
         for (let i = 1; i < n; i+= 2) {
-            const prev = expCopy.pop();
             if (exp[i] === op) {
-                expCopy.push(calc(prev, exp[i + 1], op));
+                expCopy.push(calc(exp[i - 1], exp[i + 1], op));
+                n--;
             } else {
-                expCopy.push(prev);
+                expCopy.push(exp[i - 1]);
                 expCopy.push(exp[i]);
-                expCopy.push(exp[i + 1]);
             }
         }
+        console.log(expCopy, op)
         return expCopy
     }
 	
     const combi = (d, order, visited) => {
         if (d === 3) {
-			max = Math.max(max, Math.abs(order.map(x=>OP[x]).reduce(doCalc, expressionArr)[0]));
-            return;
+            console.log(order.map(x=>OP[x]).reduce(doCalc, expressionArr));
         }
         for (let i = 0; i < 3; i++) if (!(visited & 1 << i)) combi(d + 1, [...order, i], visited | 1 << i);
     }
     
     combi(0, [], 0);
-    
-    return max;
 }
