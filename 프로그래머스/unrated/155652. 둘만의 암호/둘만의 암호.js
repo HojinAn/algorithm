55ec028425ab576c7e29e12817ecded59aeaacbb
안호진 <47904304+HojinAn@users.noreply.github.com>
2023-04-10 14:20:29 +0900

    [unrated] Title: 둘만의 암호, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
@@ -1,12 +1,5 @@
 function solution(s, skip, index) {
-    const alphabets = 'abcdefghijklmnopqrstuvwxyz'.split('');
-    const alphabetMap = new Map(alphabets.map((ch, i) => [ch, i]));
-    skip.split('').forEach((ch) => alphabetMap.delete(ch));
-    const newAlphabets = [...alphabetMap.keys()];
-    const newAlphabetMap = new Map(newAlphabets.map((ch, i) => [ch, i]));
-    const size = newAlphabets.length;
-    return s.split('').map((ch) => {
-        const idx = newAlphabetMap.get(ch);
-        return newAlphabets[(idx + index) % size];
-    }).join('');
+    const alphabets = 'abcdefghijklmnopqrstuvwxyz'.split('').filter((ch) => !skip.includes(ch));
+    const size = alphabets.length;
+    return s.split('').map((ch) => alphabets[(alphabets.indexOf(ch) + index) % size]).join('');
 }
