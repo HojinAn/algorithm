d8ee514a74aa2233ba4835b6498f30e8a7e4341a
안호진 <47904304+HojinAn@users.noreply.github.com>
2023-05-28 20:52:53 +0900

    [Silver II] Title: 용돈 관리, Time: 248 ms, Memory: 18680 KB -BaekjoonHub

---
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.ts
@@ -0,0 +1,37 @@
+import * as fs from 'fs';
+const [s1, ...inp] = fs
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+const [N, M] = s1.split(' ').map(Number);
+const budgets = inp.map(Number);
+
+const solution = (N: number, M: number, budgets: number[]) => {
+  let left = Math.max(...budgets);
+  let right = budgets.reduce((acc, cur) => acc + cur, 0);
+  let result = 0; 
+
+  while (left <= right) {
+    const K = Math.floor((left + right) / 2);
+    let sum = 0;
+
+    const cnt = budgets.reduce((cnt, el) => {
+      if (sum + el > K) {
+        cnt++;
+        sum = 0;
+      }
+      sum += el;
+      return cnt;
+    }, 1);
+
+    if (cnt <= M) {
+      result = K;
+      right = K - 1;
+    } else left = K + 1;
+  }
+  
+  console.log(result);
+};
+
+solution(N, M, budgets);
